77f48dafc6856d2bff3a3cd1702a83d0803fe45c
Jacob A Rose <31635761+JacobARose@users.noreply.github.com>
2025-04-17 23:01:28 -0400

    Update download_plantclef2025.py
    Removed irrelevant commented out code

--- plantclef/datasets/download_plantclef2025.py
+++ plantclef/datasets/download_plantclef2025.py
@@ -25,10 +25,6 @@ if __name__ == "__main__":
     # Stream the file directly from URL and upload to S3
     url = "https://lab.plantnet.org/LifeCLEF/PlantCLEF2024/single_plant_training_data/PlantCLEF2024singleplanttrainingdata_800_max_side_size.tar"
 
-    # AWS client
-    # s3_client = boto3.client("s3")
-    # bucket_name = "plantclef2025"
-    # object_name = "PlantCLEF2024singleplanttrainingdata_800_max_side_size.tar"
     file_path = "/tmp/largefile.tar"  # Temporary local path for the file
 
     ###############################
@@ -41,13 +37,3 @@ if __name__ == "__main__":
         exit(1)
 
     # # Step 2: Upload the file to S3
-    # if upload_local_file_to_s3(file_path, bucket_name, object_name, config):
-    #     print(f"File uploaded successfully to s3://{bucket_name}/{object_name}.")
-    # else:
-    #     print("Failed to upload the file.")
-    #     exit(1)
-
-# Clean up the temporary file (optional)
-# os.remove(file_path)
-# print(f"Temporary file {file_path} removed.")
-# print("File uploaded successfully!")
