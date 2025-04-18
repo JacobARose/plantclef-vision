9eb12860f937f320a4aa51e9236f9ed9c51284e5
Jacob A Rose <JacobRose.Codes@gmail.com>
2025-04-18 08:32:32 +0000

    Added quick TODO note to data downloading script

--- scripts/run_setup_plantclef2025_data.sh
+++ scripts/run_setup_plantclef2025_data.sh
@@ -5,6 +5,12 @@
 #              configuration parameters for setting up PlantCLEF 2025 data.
 # Author: Jacob A Rose
 # Date: Monday April 14th, 2025
+
+# [TODO] -- Add a custom registry.json file to the plantclef-vision/data directory to allow consistent mapping between source URL/filename and target local directory name
+# e.g. {"PlantCLEF2024singleplanttrainingdata_800_max_side_size":
+#           "small_singleplanttrainingdata"}
+
+
 #===============================================================================
 
 # Exit immediately if a command exits with a non-zero status
