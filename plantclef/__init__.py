9109234c71d351f9f1a66bce34f6eadf16ef4116
Jacob A Rose <JacobRose.Codes@gmail.com>
2025-04-29 09:22:50 +0000

    Continued efforts to fix pip install submodule resolution bug

--- plantclef/__init__.py
+++ plantclef/__init__.py
@@ -1,6 +1,21 @@
-from . import (
-    datasets,
-    embed,
-    faiss,
-    torch
-)
+from . import datasets, embed, faiss, torch, config, model_setup, plotting, serde
+
+import sys
+
+
+__all__ = [
+    "datasets",
+    "embed",
+    "faiss",
+    "torch",
+    "config",
+    "model_setup",
+    "plotting",
+    "serde",
+]
+
+print(f"Currently in file: {__file__}")
+print(f"sys.path = {sys.path}")
+print(f"__name__ = {__name__}")
+print(f"__package__ = {__package__}")
+print(f"dir() = {dir()}")
