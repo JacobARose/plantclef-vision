2fca0de50ebaeaeceb041920f9acc13421de5d32
Jacob A Rose <JacobRose.Codes@gmail.com>
2025-04-30 05:21:56 +0000

    Still completely stuck working on this broken pip install error

--- plantclef/__init__.py
+++ plantclef/__init__.py
@@ -1,21 +1,21 @@
-from . import datasets, embed, faiss, torch, config, model_setup, plotting, serde
+# from . import datasets, embed, faiss, torch, config, model_setup, plotting, serde
 
-import sys
+# import sys
 
 
-__all__ = [
-    "datasets",
-    "embed",
-    "faiss",
-    "torch",
-    "config",
-    "model_setup",
-    "plotting",
-    "serde",
-]
+# __all__ = [
+#     "datasets",
+#     "embed",
+#     "faiss",
+#     "torch",
+#     "config",
+#     "model_setup",
+#     "plotting",
+#     "serde",
+# ]
 
-print(f"Currently in file: {__file__}")
-print(f"sys.path = {sys.path}")
-print(f"__name__ = {__name__}")
-print(f"__package__ = {__package__}")
-print(f"dir() = {dir()}")
+# print(f"Currently in file: {__file__}")
+# print(f"sys.path = {sys.path}")
+# print(f"__name__ = {__name__}")
+# print(f"__package__ = {__package__}")
+# print(f"dir() = {dir()}")
