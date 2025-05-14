5b3dd0fa07f3ac59bd7d05c2e1b33842f04a0faa
Jacob A Rose <JacobRose.Codes@gmail.com>
2025-05-14 08:11:07 +0000

    Further development of the `data_catalog.py`. Can now run `python data_catalog.py` on cmd line to see a list of available datasets and their configs.

--- plantclef/pytorch/data_catalog.py
+++ plantclef/pytorch/data_catalog.py
@@ -4,9 +4,16 @@ Created on Tuesday May 13th, 2025
 Created by: Jacob A Rose
 
 
+Provides Config dataclasses for specific datasets, plus helper functions to load them into HFPlantDatasetDicts.
+
+
+Run script to see available datasets and their configs.
+
+
 """
 
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+from plantclef.pytorch.data import HFPlantDatasetDict
 
 
 # data_subset_paths = {
@@ -25,13 +32,13 @@ class SubsetDataConfig:
 
 @dataclass
 class DatasetConfig:
-    subset_paths: SubsetDataConfig
+    paths: SubsetDataConfig
     x_col: str
     y_col: str
 
 
 PlantCLEF2024DatasetConfig = DatasetConfig(
-    subset_paths=SubsetDataConfig(
+    paths=SubsetDataConfig(
         **{
             "train": "/teamspace/studios/this_studio/plantclef-vision/data/hf/plantclef2025/single_label_train_val_test/shortest_edge_308/train",
             "val": "/teamspace/studios/this_studio/plantclef-vision/data/hf/plantclef2025/single_label_train_val_test/shortest_edge_308/val",
@@ -49,4 +56,39 @@ def make_dataset_from_config(cfg: DatasetConfig, **kwargs):
     """
     from plantclef.pytorch.data import HFPlantDatasetDict
 
-    return HFPlantDatasetDict(**PlantCLEF2024DatasetConfig(), **kwargs)
+    return HFPlantDatasetDict(**asdict(cfg), **kwargs)
+
+
+###################################################################
+###################################################################
+
+###################################################################
+###################################################################
+
+
+available_datasets_configs = {"plantclef2024": PlantCLEF2024DatasetConfig}
+
+
+def make_dataset(name: str = "", **kwargs) -> HFPlantDatasetDict:
+    """
+    Main function to create an HFPlantDatasetDict using a known DatasetConfig by querying with a str name.
+    """
+
+    if name not in available_datasets_configs:
+        raise ValueError(
+            f"Dataset {name} not found in available datasets: {available_datasets_configs.keys()}"
+        )
+    cfg = available_datasets_configs[name]
+
+    return make_dataset_from_config(cfg, **kwargs)
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    print(f"Available datasets: {available_datasets_configs.keys()}")
+
+    for name, cfg in available_datasets_configs.items():
+        print(f"Dataset: {name}")
+        pprint(cfg)
+        print()
