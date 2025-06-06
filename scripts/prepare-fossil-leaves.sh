17ba6ecb268e39e13a9bd95afd2794361b6b53f2
Jacob A Rose <JacobRose.Codes@gmail.com>
2025-06-06 04:06:40 +0000

    Added docstring for `prepare-fossil-leaves.sh` to be filled out shortly.

---
+++ scripts/prepare-fossil-leaves.sh
@@ -0,0 +1,21 @@
+"""
+Created on Thursday June 5th, 2025
+Created by Jacob A Rose
+
+
+The directory of zip files associated with fossil-leaves should be hosted in an AWS S3 bucket, and mounted to the lightning studio file system at the following path:
+`/teamspace/s3_connections/fossil-leaves/`
+
+The contents should be:
+```
+Extant_Leaves_A-E_v2.0.zip
+Extant_Leaves_F-O_v2.0.zip
+Extant_Leaves_P-Z_v2.0.zip
+Florissant_Fossil_v2.0.zip
+General_Fossil_uncropped_v2.0.zip
+General_Fossil_v2.0.zip
+supplemental_data_v2.0.zip
+```
+
+
+"""
